4f4e412ae656665d703bc70bfd8d88e7ffc617e9
Tushar Gandhi <tgandhi@IT-C02QRA2NG8WP.local>
2019-01-07 16:05:50 +0530

    enhancements

--- src/main/java/service/add/controller/AddController.java
+++ src/main/java/service/add/controller/AddController.java
@@ -1,5 +1,7 @@
 package service.add.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,12 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AddController {
 
+	
+	private final Logger logger = LoggerFactory.getLogger(AddController.class);
+	
+	
 	@RequestMapping(value="/api/v1/add", method = RequestMethod.GET)
 	public double add(
 			@RequestParam(value="value1", required=true) double value1,
 			@RequestParam(value="value2", required=true) double value2
 			) {
 
+		logger.info("Request received for "+value1+"+"+value2);
 		return value1+value2;
 	}
 	
